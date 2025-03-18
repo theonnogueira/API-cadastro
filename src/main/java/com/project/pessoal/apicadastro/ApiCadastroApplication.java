e70ea2a2f33e10baa331eaa79013348f4afb9ca1
Theo Nogueira Medeiros <tnogueim@emeal.nttdata.com>
2025-03-18 15:16:27 -0300

    first committ

---
+++ src/main/java/com/project/pessoal/apicadastro/ApiCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.project.pessoal.apicadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/pessoal/apicadastro/ApiCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pessoal.apicadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
