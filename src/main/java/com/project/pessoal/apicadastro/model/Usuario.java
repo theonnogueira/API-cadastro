7775bf0e32e13773389c65a69e304703890a1c58
Fernando Silva <fernandoadmti@hotmail.com>
2025-03-18 15:38:07 -0300

    criacao da entidade usuario

---
+++ src/main/java/com/project/pessoal/apicadastro/model/Usuario.java
@@ -0,0 +1,115 @@
+package com.project.pessoal.apicadastro.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String nome;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String telefone;
+
+    @NotNull
+    private String documento;
+
+    @NotNull
+    private String senha;
+
+    @NotNull
+    private char status;
+
+    @NotNull
+    @Column(name = "data_nascimento")
+    private LocalDate dataNascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public char getStatus() {
+        return status;
+    }
+
+    public void setStatus(char status) {
+        this.status = status;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", documento='" + documento + '\'' +
+                ", senha='" + senha + '\'' +
+                ", status=" + status +
+                ", dataNascimento=" + dataNascimento +
+                '}';
+    }
+}
